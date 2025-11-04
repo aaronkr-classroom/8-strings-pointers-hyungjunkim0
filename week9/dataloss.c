50116592914ec6df44063c275ae17fa9ad4d64b6
ththfql <145094239+ththfql@users.noreply.github.com>
2025-11-04 16:56:11 +0900

    Hello

---
+++ week9/dataloss.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main(void) {
+	unsigned int x = 0x12345678;
+	long lg = 2147483648; //롱의 최대값
+	double xl = 123456789123.456789;
+
+	printf("\nUint: %d\n", x);
+	printf("Uint -> short: %d\n", (short)x);
+	printf("Uint -> char: %d\n", (char)x);
+
+	printf("\nLong: %d\n", lg);
+	printf("Long -> short: %d\n", (short)lg);
+	printf("Long -> char: %d\n", (char)lg);
+
+	printf("\nDbl: %lf\n", xl);
+	printf("Dbl -> short: %d\n", (short)xl);
+	printf("Dbl -> char: %d\n", (char)xl);
+
+
+	return 0;
+}
---
+++ week9/endian.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+void check_endian() {
+	//첫 메모리 주소가 0X12이면 빅 엔디언이고
+	//0X78이면 리틀 엔디언이다.
+	unsigned int num = 0x123456789; //16진법 (4바이트)
+	unsigned char* byte_ptr = (unsigned char*)&num;
+
+	//모든 바이트 주소와 값을 출력하기
+	for (int i = 0; i < sizeof(num); i++) {
+		printf("%p   0x%02x\n", (byte_ptr + i), byte_ptr[i]);
+	}
+
+//첫 바이트 주소를 확인하고 엔디어니스 출력하기
+if (byte_ptr[0] == 0x12) {
+	printf("\nThis is BIG-endian!");
+}else if(byte_ptr[0]== 0x78){
+	printf("\nthis is _little_endian...");
+}
+else {
+	printf("\nUnKnown endian!ㅠㅠ");
+}
+}
+int main(void) {
+	check_endian();
+	return 0;
+}
